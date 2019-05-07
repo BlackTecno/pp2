08f9cc067560109b25ff0da51f40cbf325c4a908
BlackTecno <Blacktecnox@gmail.com>
2019-05-07 15:16:05 -0500

    Add files via upload

--- oneL.c
+++ oneL.c
@@ -84,7 +84,7 @@ void mult(int size, int *threads) {
 
 				#pragma omp for schedule(static)
 				for (k = 0; k < size; k++) {
-					printf("Here we are %d, %d, %d", i, j, k);
+					printf("Here we are %d, %d, %d\n", i, j, k);
 					c[i][j] += a[i][k] * b[k][j];
 
 				}
