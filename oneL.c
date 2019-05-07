b34b4036cf85253b22c16ec3ebc3bb52f9857c31
BlackTecno <Blacktecnox@gmail.com>
2019-05-07 13:40:29 -0500

    Add files via upload

--- oneL.c
+++ oneL.c
@@ -25,14 +25,13 @@ int main(int argc, char **argv) {
 		printf("Enter <size of Matrix>, <Nuber of Threads>:");
 		scanf("%d, %d", size, threads);
 
-		if (size > MAX_SIZE || size <= 0) {
-			printf("Incorrect size for matix, please input a number between 1 and %d\n", MAX_SIZE);}
-		else {
-			check = 1; }
-
-
-		srand(time(NULL));
+		if (size > MAX_SIZE || size <= 0) 
+			printf("Incorrect size for matix, please input a number between 1 and %d\n", MAX_SIZE);
+		else 
+			check = 1; 
 	}
+
+	srand(time(NULL));
 
 
 	//MAKE MATRIX
